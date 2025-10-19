f217c2527b048337b35a86fd44072f5808a4a040
josh098741 <morongemokaya@gmail.com>
2025-10-19 15:40:30 +0300

    Additional commits to the completed Home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,19 @@ function Home(){
 
     return(
         <div className="border h-[700px] border-slate-900">
-            <div></div>
+            <div className="m-5 h-[610px] p-1">
+                <div className="flex h-1/2">
+                    <div className="border border-slate-500 mr-1 w-1/2">
+                        <div>Graph Container</div>
+                    </div>
+                    <div className="border border-slate-500 w-1/2">
+                        <div>Expense Calculator</div>
+                    </div>
+                </div>
+                <div className="h-1/2 border my-1 border-slate-500">
+                    <div>Transactions</div>
+                </div>
+            </div>
         </div>
     );
 }
