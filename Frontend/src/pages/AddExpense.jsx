6d12e248ea2a85fe95cb555b8f5b809de6ce73e9
josh098741 <morongemokaya@gmail.com>
2025-10-19 17:45:45 +0300

    Additional commits

--- Frontend/src/pages/AddExpense.jsx
+++ Frontend/src/pages/AddExpense.jsx
@@ -4,7 +4,50 @@
 function AddExpense(){
 
     return(
-        <div>Add Expense</div>
+        <div className="h-[700px] border border-slate-700">
+            <div className="border flex flex-col items-center justify-center">
+                <div>Expenses And Income</div>
+                <form>
+                    <div className="flex flex-col">
+                        <div className="flex flex-col">
+                            <label>Title</label>
+                            <input type="text" className="border border-slate-900"/>
+                        </div>
+                        
+                        <div className="flex flex-col">
+                            <label>Description</label>
+                            <textarea className="border border-slate-900" />
+                        </div>
+                        
+                        <div className="flex flex-col">
+                            <label>Money</label>
+                            <input type="number" className="border border-slate-900 h-10 w-96 outline-none" />
+                        </div>
+                        
+                        <div className="flex justify-center items-center gap-5 text-center">
+                            <div className="inline-block">
+                                <label>
+                                    <input type="radio"/>
+                                    credit
+                                </label>
+                            </div>
+
+                            <div className="inline-block">
+                                <label>
+                                    <input type="radio" />
+                                    debit
+                                </label>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        
+                    </div>
+                    <div></div>
+                </form>
+            </div>
+        </div>
     );
 }
 
