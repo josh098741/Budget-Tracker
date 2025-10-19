0649c191ad5bab6c983e2e3dc35f89debea52f11
josh098741 <morongemokaya@gmail.com>
2025-10-19 23:07:09 +0300

    Additional commit to my home page with addition of backend functionality

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,41 @@
-
-
+import axios from 'axios'
+import React,{ useState,useEffect } from 'react'
+import { Edit,Trash } from 'lucide-react'
+import toast from 'react-hot-toast'
+import { Link } from 'react-router-dom'
 
 function Home(){
+
+    const [expense, setExpense] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const handleDelete = async (id) => {
+        try{
+            await axios.delete(`http://localhost:5000/api/expense/${id}`)
+            const updatedExpense = expense.filter((item) => item._id !== id)
+            setExpense(updatedExpense)
+        }catch(error){
+            console.log("Error in deleting expense",error)
+            toast.error("Failed to delete the expense")
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                setLoading(true)
+                const response = await axios.get("http://localhost:5000/api/expense")
+                console.log(response.data)
+                setExpense(response.data)
+            }catch(error){
+                console.log(error)
+            }finally{
+                setLoading(false)
+            }
+            
+        }
+        fetchData()
+    },[])
 
     return(
         <div className="border h-[700px] border-slate-900">
@@ -14,8 +48,32 @@ function Home(){
                         <div>Expense Calculator</div>
                     </div>
                 </div>
-                <div className="h-1/2 border my-1 border-slate-500">
-                    <div>Transactions</div>
+                <div className="h-1/2 border my-1 border-slate-500 flex flex-wrap">
+                    {
+                        loading ? <div>Loading</div> 
+                        : expense && expense.length > 0 ? 
+                        expense.map((item) => (
+                            <div key={item._id} className="border m-3 w-40 h-20 border-slate-900 rounded text-xs grid grid-cols-2">
+                                <div className="border-r border-slate-500">
+                                    <h1 className="text-center">{item.title}</h1>
+                                    <p >{item.amount}</p>
+                                    <p className={item.type === 'debit' ? 'text-red-800':'text-green-500'}>{item.type}</p>
+                                </div>
+                                <div className="flex flex-col justify-between">
+                                    <div><p>{item.description}</p></div>
+                                    <div className="flex justify-between">
+                                        <button  onClick={() => handleDelete(item._id)}><Trash className="hover:text-red-500 transition-all" /></button>
+                                        <Link>
+                                            <button><Edit className="hover:text-green-500 transition-all" /></button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                        
+                        : <div></div>
+                    }
+                    
                 </div>
             </div>
         </div>
