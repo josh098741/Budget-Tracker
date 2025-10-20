13d5e90546eab02c4e6b489ad54ad6e6d9875a9a
josh098741 <morongemokaya@gmail.com>
2025-10-20 11:19:10 +0300

    Additional commits to my expense calculator

--- Frontend/src/components/ExpenseCalculator.jsx
+++ Frontend/src/components/ExpenseCalculator.jsx
@@ -5,7 +5,29 @@ function ExpenseCalculator({ expense }){
 
     return(
         <div className={`w-full h-full`}>
-            Expenses
+            <div className="text-center text-xl">Expenses</div>
+            <div className="flex justify-between mx-10 my-5">
+                <div>
+                    <h1>Credit</h1>
+                    {
+                        expense && expense.length > 0 ? 
+                        expense.map((item) => (
+                            <div key={item._id}>{item.type === "credit" ? `${item.amount} KSH` : ""}</div>
+                        ))
+                        : <div>No Expense</div>
+                    }
+                </div>
+                <div>
+                    <h1>Debit</h1>
+                    {
+                        expense && expense.length > 0 ?
+                        expense.map((item) => (
+                            <div key={item._id}>{item.type === "debit" ? `${item.amount} KSH` : ""}</div>
+                        ))
+                        : <div>No expense</div>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
